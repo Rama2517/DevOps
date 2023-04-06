1c1277fb04d22901164bf12698b2cfe9c7b3ac3c
Rama2517 <127804264+Rama2517@users.noreply.github.com>
2023-04-06 01:30:23 -0700

    sccm

---
+++ Install_SCCM_Client_NON-PSL_Domain.bat
@@ -0,0 +1,1 @@
+Ccmsetup.exe /UsePKICert /NoCRLCheck /mp:https://hj-cmp.persistent.co.in SMSSITECODE=HJP SMSMP=https://hj-cmp.persistent.co.in FSP=hj-cmp.persistent.co.in SMSLP=hj-cmp.persistent.co.in DNSSUFFIX=persistent.co.in
---
+++ Install_SCCM_Client_PSL_Domain.bat
@@ -0,0 +1,1 @@
+Ccmsetup.exe SMSSITECODE=HJP SMSMP=hj-cmp.persistent.co.in FSP=hj-cmp.persistent.co.in SMSMP=hj-cmp.persistent.co.in
